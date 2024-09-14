cbbc4140255b958c0fd465e601538f26af223c6c
PolinaT <frozen.mind070@gmail.com>
2024-09-14 12:12:28 +0300

    Добавьте файлы проекта.

---
+++ Kotlin_testcase/ICurrency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotlin_testcase
+{
+    internal interface ICurrency
+    {
+        void ChangeExchangeRates();
+        void IncreaseCapacity(float money);
+        void DecreaseCapacity(float money);
+    }
+}
